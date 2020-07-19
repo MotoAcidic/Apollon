88d0008d96723e871b6af5977de5cd1343c422d0
Tfinch <travisfinch01@gmail.com>
2020-07-19 17:53:57 -0400

    Remove backport Consensus

--- src/qt/governancepage.cpp
+++ src/qt/governancepage.cpp
@@ -88,10 +88,9 @@ void GovernancePage::updateProposalList()
         pindexPrev = chainActive.Tip();
     }
     if (!pindexPrev) return;
-    const int nBlocksPerCycle = Params().GetConsensus().nBudgetCycleBlocks;
-    int nBlockStart = pindexPrev->nHeight - pindexPrev->nHeight % nBlocksPerCycle + nBlocksPerCycle;
+    int nBlockStart = pindexPrev->nHeight - pindexPrev->nHeight % Params().GetBudgetCycleBlocks() + Params().GetBudgetCycleBlocks();
     int nBlocksLeft = nBlockStart - pindexPrev->nHeight;
-    int nBlockEnd = nBlockStart + nBlocksPerCycle - 1;
+    int nBlockEnd = nBlockStart + Params().GetBudgetCycleBlocks() - 1;
     int mnCount = mnodeman.CountEnabled(ActiveProtocol());
 
     for (CBudgetProposal* pbudgetProposal : proposalsList) {
